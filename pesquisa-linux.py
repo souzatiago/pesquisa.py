725a3435738c4fcea7bca6a412479c6f2c5cff35
souzatiago <77245299+souzatiago@users.noreply.github.com>
2021-08-21 13:31:20 -0300

    Create pesquisa-linux.py

---
+++ pesquisa-linux.py
@@ -0,0 +1,7 @@
+import subprocess as s
+
+while True:
+    print('Pesquisar no Google ou digitar URL')
+    entrada = str(input())
+    print('Aguarde, Carregando...')
+    s.Popen(['xdg-open', 'http://www.google.com/search?q={}'.format(entrada)])
