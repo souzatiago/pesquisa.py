c0e7d01c96ffd65d7b276107a707c4db1936c380
souzatiago <77245299+souzatiago@users.noreply.github.com>
2021-08-21 13:28:20 -0300

    Update and rename pesquisa.py to pesquisa-windows.py

--- pesquisa-windows.py
+++ pesquisa-windows.py
@@ -1,10 +1,7 @@
 import os
-import time
 
 while True:
-
     print('Pesquisar no Google ou digitar URL')
     pesquisa = str(input())
     print('Aguarde, Carregando... ')
-    time.sleep(1)
     os.startfile('https://www.google.com/search?q={}'.format(pesquisa))
