7f95b775c3f80ae716312e22ea999ed8764328f8
souzatiago <77245299+souzatiago@users.noreply.github.com>
2021-08-27 13:24:17 -0300

    Update pesquisa-linux.py

--- pesquisa-linux.py
+++ pesquisa-linux.py
@@ -2,6 +2,6 @@ import subprocess as s
 
 while True:
     print('Pesquisar no Google ou digitar URL')
-    pesquisa = str(input())
+    entrada = str(input())
     print('Aguarde, Carregando...')
     s.Popen(['xdg-open', 'http://www.google.com/search?q={}'.format(entrada)])
